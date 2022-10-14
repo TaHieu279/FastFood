7dd9be8038d8e9c390ed1a77982efc8a101ff5de
AnhNguyet23 <thiennnannn23062001@gmail.com>
2022-10-14 15:20:41 +0700

    #5 - Anh nguyet create user class

---
+++ FastFood/app/src/main/java/com/tavanhieu/fastfood/my_class/User.java
@@ -0,0 +1,75 @@
+package com.tavanhieu.fastfood.my_class;
+
+import android.net.Uri;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String uid, userName, email, dateOfBirth, address;
+    private Uri imgAvatar;
+
+    public User() {}
+
+    public User(String userName, String email, String uid) {
+        this.userName = userName;
+        this.email = email;
+        this.uid = uid;
+    }
+
+    public User(String uid, String userName, String email, String dateOfBirth, String address, Uri imgAvatar) {
+        this.uid = uid;
+        this.userName = userName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.address = address;
+        this.imgAvatar = imgAvatar;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Uri getImgAvatar() {
+        return imgAvatar;
+    }
+
+    public void setImgAvatar(Uri imgAvatar) {
+        this.imgAvatar = imgAvatar;
+    }
+}
